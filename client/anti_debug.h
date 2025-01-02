76e885ac1b383d8ee817c55318d7db640fd8892a
Rishabh Dangwal <admin@theprohack.com>
2025-01-02 18:46:10 +0530

    changed variable type in header

--- client/anti_debug.h
+++ client/anti_debug.h
@@ -15,8 +15,8 @@
 // Function prototypes
 void anti_debug_ptrace();     // Detect debugger via ptrace (Linux only)
 void anti_debug_proc();       // Detect debugger via /proc/self/status
-void anti_debug_timing();     // Detect debugger via timing discrepancies
-// void anti_debug_signals();  Detect debugger via signal handling, disabling it for now since it is causing issues on Mac
+int anti_debug_timing();     // Detect debugger via timing discrepancies
+void anti_debug_signals();  Detect debugger via signal handling, disabling it for now since it is causing issues on Mac
 void anti_debug_ppid();       // Detect debugger via parent process ID
 void anti_debug_env();        // Detect debugger via environment variables
 void anti_debug_checksum();   // Detect debugger via checksum verification
